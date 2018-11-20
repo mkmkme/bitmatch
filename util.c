63b4ad95138a78dbe810943811b616b32e4a9b31
Reverend Homer <mk.43.ecko@gmail.com>
2018-11-20 18:34:27 +0300

    add couple of asserts to util

--- util.c
+++ util.c
@@ -4,12 +4,16 @@ uint64_t get_range_bit(uint64_t word, uint8_t lbit, uint8_t fbit)
 {
 	const uint64_t mask = (1ull << (lbit - fbit + 1)) - 1;
 	const uint64_t val = word >> (fbit - 1);
+	assert(lbit >= 1 && lbit <= 64);
+	assert(fbit >= 1 && fbit <= 64);
 	return val & mask;
 }
 
 void set_range_bit(uint64_t *word, uint8_t lbit, uint8_t fbit, uint8_t val)
 {
 	const uint64_t mask = (1ull << (lbit - fbit + 1)) - 1;
+	assert(lbit >= 1 && lbit <= 64);
+	assert(fbit >= 1 && fbit <= 64);
 	if (val == 1)
 		*word |= (mask << (fbit - 1));
 	else
@@ -19,6 +23,8 @@ void set_range_bit(uint64_t *word, uint8_t lbit, uint8_t fbit, uint8_t val)
 void copy_range_bit(uint64_t *word, uint8_t lbit, uint8_t fbit, uint64_t val)
 {
 	const uint64_t mask = val << (fbit - 1);
+	assert(lbit >= 1 && lbit <= 64);
+	assert(fbit >= 1 && fbit <= 64);
 	set_range_bit(word, lbit, fbit, 0);
 	*word |= mask;
 }
