afb59175e116b6f03f9c1042c0b7ef4489ee7dd8
Reverend Homer <mk.43.ecko@gmail.com>
2018-11-20 18:35:37 +0300

    bitset: fix shift overflow

--- bitset.c
+++ bitset.c
@@ -27,7 +27,8 @@ void bitset_set_bits(struct bitset *bs, uint64_t lbit, uint64_t fbit, uint64_t v
 	const uint64_t fcell = (fbit-1) / 64; // cell containing first bit
 
 	if (lcell == fcell) {
-		copy_range_bit(&bs->data[lcell], lbit, fbit, val);
+		DBG("lbit=%lu, fbit=%lu, val=%lu, lcell=%lu\n", lbit - 64 * lcell, fbit - 64 * lcell, val, lcell);
+		copy_range_bit(&bs->data[lcell], lbit - 64 * lcell, fbit - 64 * lcell, val);
 	} else {
 		// 5...11
 		//       3 1   8  5
